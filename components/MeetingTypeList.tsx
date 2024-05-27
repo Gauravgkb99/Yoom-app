47881f7e4b1df53544ac48fece4ae913e478df33
Gauravgkb99 <gauravkingbudhlakoti9999@gmail.com>
2024-05-27 23:26:54 +0530

    added more functionality and attributes

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,7 @@ import { useToast } from './ui/use-toast'
 import { Textarea } from './ui/textarea'
 import ReactDatePicker from 'react-datepicker'
 import { Loader } from 'lucide-react'
+import { Input } from './ui/input'
 const MeetingTypeList = () => {
   
   const router = useRouter();
@@ -160,6 +161,17 @@ const MeetingTypeList = () => {
           buttonText="Start Meeting"
           handleClick = {createMeeting}
         />
+
+        <MeetingModal
+          isOpen={meetingState === 'isJoiningMeeting'}
+          onClose={() => setMeetingState(undefined)}
+          title="Type meeting id"
+          className='text-center'
+          buttonText="Join Meeting"
+          handleClick = {() => router.push(values.link)}
+        >
+          <Input placeholder='Meeting link' className='border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0' onChange={(e) => setValues({...values, link: e.target.value})} />
+        </MeetingModal>
     </section>
   )
 }
